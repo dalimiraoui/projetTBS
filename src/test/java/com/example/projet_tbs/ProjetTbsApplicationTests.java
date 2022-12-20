62ceec534b8b0dd04b57e4bf8047677742912a51
dalimiraoui <dalimiraoui48@gmail.com>
2022-12-20 21:26:26 +0100

    Initial commit

---
+++ src/test/java/com/example/projet_tbs/ProjetTbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_tbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
